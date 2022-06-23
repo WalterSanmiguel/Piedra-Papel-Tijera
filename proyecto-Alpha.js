625532034937be98568765bd56270b6406782134
WalterSanmiguel <waltermonito1@gmail.com>
2022-06-23 18:49:21 +1200

    se cargaron imagenes

---
+++ proyecto-Alpha.js
@@ -0,0 +1,12 @@
+const list = ["Piedra", "Papel", "Tijera"];
+
+
+function machine () {const random = list[Math.floor(Math.random()*list.length)];
+    
+    document.getElementById("parruf").innerText = random}
+     var P1 = 0
+     var CPU = 0
+     if (random = papel) {
+        alert("hey, you won")
+        //borrar este mensaje
+     }
